719927e1558c238734bc7c8d3714d5d7155366d2
“nicolasmuneram” <nmunera@eafit.edu.co>
2014-07-07 00:50:49 -0300

    Challenge solved! :D

--- UITableViewController Challenge Solution/NMTableViewController.h
+++ UITableViewController Challenge Solution/NMTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  NMViewController.h
+//  NMTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Nicolás Múnera on 7/07/14.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface NMViewController : UIViewController
+@interface NMTableViewController : UITableViewController
 
 @end
